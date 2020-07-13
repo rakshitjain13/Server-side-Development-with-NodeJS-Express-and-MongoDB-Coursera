d60685ac5b6b20edc1066c48afc42aaacc46bc68
Rakshit Jain <rakshitdhoka13@gmail.com>
2020-07-14 00:07:59 +0530

    Mongose API server Part-1

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
 var app = express();
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+    console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
