fc9fd2b8344564d6e4c65bc911dfb32d119ffcd2
Rakshit Jain <rakshitdhoka13@gmail.com>
2020-08-07 00:42:16 +0530

    Cookies

--- app.js
+++ app.js
@@ -32,10 +32,11 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 
 function auth (req, res, next) {
   console.log(req.headers);
+  if(!req.signedCookies.user){
   var authHeader = req.headers.authorization;
   if (!authHeader) {
       var err = new Error('You are not authenticated!');
@@ -49,12 +50,23 @@ function auth (req, res, next) {
   var user = auth[0];
   var pass = auth[1];
   if (user == 'admin' && pass == 'password') {
+      res.cookie('user','admin',{signed: true});
       next(); // authorized
   } else {
       var err = new Error('You are not authenticated!');
       res.setHeader('WWW-Authenticate', 'Basic');      
       err.status = 401;
       next(err);
+  }
+  }else{
+      if(req.signedCookies.user==='admin'){
+        next();
+      }else{
+        var err = new Error('You are not authenticated!');
+        err.status = 401;
+        next(err);
+      }
+
   }
 }
 
