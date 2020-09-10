b86c694e202d0570c9ea118ba65fd47e69e2341c
Rakshit Jain <rakshitdhoka13@gmail.com>
2020-09-11 02:33:45 +0530

    OAuth2 Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
-}
+  secretKey: "12345-67890-09876-54321",
+  mongoUrl: "mongodb://localhost:27017/conFusion",
+  facebook: {
+    clientId: "956359178180803",
+    clientSecret: "552644ebddd5b4a9e101d5df41071933",
+  },
+};
